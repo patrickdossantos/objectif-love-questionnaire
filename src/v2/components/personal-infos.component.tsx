ba6cf05dc83c1c534f88b896432e32c7dac0367c
Patrick DOS SANTOS <patrick.dossantos2@gmail.com>
2024-09-15 11:43:17 +0200

    update: finished all the design part

--- src/v2/components/personal-infos.component.tsx
+++ src/v2/components/personal-infos.component.tsx
@@ -1,5 +1,5 @@
 import { PersonalInfos, Situation } from '../../types.ts';
-import { useCallback, useState } from 'react';
+import { useRef, useState } from 'react';
 import { SITUATIONS } from '../../constants.ts';
 import { cn } from '../../utils.ts';
 
@@ -8,37 +8,59 @@ type Props = {
   previousValue: PersonalInfos | undefined
 }
 
-export default function PersonalInfosComponent({ setPersonalInfos, previousValue }: Props) {
-  const [name, setName] = useState(previousValue?.name ?? '')
-  const [email, setEmail] = useState(previousValue?.email ?? '')
+const PersonalInfosHeader = () => (<>
+  <div style={{textAlign: "justify", fontSize: "1rem"}} className="mb-3 fs-3 fw-bold">
+    Ce qui suit pourrait bien changer la façon dont vous voyez vos relations. Prêt(e) ?
+  </div>
+  <div className="text-center mb-5">
+    <img className="img-fluid ratio-16x9 w-50" alt="circle"
+         src="https://app.evalandgo.com/files/img_user/admin_550602/Capture%20d%E2%80%99e%CC%81cran%202024-07-08%20a%CC%80%2015_02_08(1).png"/>
+  </div>
+</>)
+
+export default function PersonalInfosComponent({setPersonalInfos, previousValue}: Props) {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const acceptRef = useRef<HTMLInputElement>(null);
   const [status, setStatus] = useState<Situation | undefined>(previousValue?.situation)
   const [errors, setErrors] = useState<string[]>([])
 
-  const validate = useCallback(() => {
-    const isNameInvalid = !name.length
-    const isEmailInvalid = email.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/) === null
+  const validate = () => {
+    const isNameInvalid = !nameRef.current?.value.length
+    const isEmailInvalid = (emailRef.current?.value ?? '').match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/) === null
     const isStatusInvalid = typeof status === 'undefined'
+    const isNotAccepted = !acceptRef.current?.checked
 
     const errors: string[] = [
       isNameInvalid ? 'Le nom doit être rempli': '',
       isEmailInvalid ? "L'email n'est pas valide": '',
       isStatusInvalid ? 'Veuillez renseigner votre situation amoureuse': '',
+      isNotAccepted ? 'Veuillez accepter le traitement de vos données personnelles': ''
     ].filter((item) => item.length > 0);
 
-    errors.length ? setErrors(errors): setPersonalInfos({ situation: status!, name, email })
-  }, [name, email, status, setErrors, setPersonalInfos]);
+    errors.length ? setErrors(errors): setPersonalInfos({ situation: status!, name: nameRef.current!.value, email: emailRef.current!.value })
+  }
 
 
   return <form>
+    <PersonalInfosHeader/>
+    {
+      errors.map((error) => (<div className="bg-red text-white mb-3 px-3 rounded" key={error}>{error}</div>))
+    }
     <div className="input-group mb-3">
       <span className="input-group-text">Email</span>
-      <input type="text" className="form-control" placeholder="Email"
-             value={email} onChange={(e) => setEmail(e.target.value)}/>
+      <input type="text" className="form-control" placeholder="Email" ref={emailRef} defaultValue={previousValue?.email} />
     </div>
     <div className="input-group mb-3">
       <span className="input-group-text">Nom</span>
-      <input type="text" className="form-control" placeholder="Prénom"
-             value={name} onChange={(e) => setName(e.target.value)}/>
+      <input type="text" className="form-control" placeholder="Prénom" ref={nameRef} defaultValue={previousValue?.name} />
+    </div>
+    <div className="form-check my-5">
+      <input className="form-check-input" type="checkbox" value="" id="acceptDataUsage" ref={acceptRef} />
+      <label className="form-check-label me-2" htmlFor="acceptDataUsage">
+        J'accepte le traitement de mes données personnelles par Objectif Love.
+      </label>
+      <a href="https://objectif-love.com/conditions-de-service" target="_blank">En savoir plus</a>
     </div>
     <div className="fw-bold mb-3">Quelle est votre situation amoureuse aujourd'hui ?</div>
     <ul className="list-group mb-5">
@@ -46,13 +68,10 @@ export default function PersonalInfosComponent({ setPersonalInfos, previousValue
         SITUATIONS.map((situation) => {
           const isSelected = situation === status
           const classnames = cn({'list-group-item': true, 'bg-red': isSelected})
-          return <li key={situation} className={classnames} onClick={() => setStatus(situation)}>{ situation }</li>
+          return <li key={situation} className={classnames} onClick={() => setStatus(situation)}>{situation}</li>
         })
       }
     </ul>
-    {
-      errors.map((error) => (<div className="bg-red text-white mb-3 px-3 rounded" key={error}>{error}</div>))
-    }
     <div className="text-end">
       <button type="button" className="btn bg-red" onClick={() => validate()}>Commencer</button>
     </div>
--- src/v2/components/results.components.tsx
+++ src/v2/components/results.components.tsx
@@ -1,75 +1,141 @@
 import { ColorAttributes, ColorSpecificities, GroupByColor } from '../../types.ts';
-import { useState } from 'react';
 import { COLORS } from '../../constants.ts';
 
 type SortResult = { percentile: number, title: string }
-const ColorResultComponent = ({ title, percentile }: SortResult) => {
-  return <>
-    <div className="progress mb-3 position-relative" style={{height: "50px"}}>
-      <div style={{height: "50px", color: "black", top: "50%", right: "50%"}}
-           className="fw-bold progress-text percentile-result">{title}: {percentile}%
-      </div>
-      <div className="progress-bar bg-red" role="progressbar" aria-valuenow={percentile} aria-valuemin={0}
-           aria-valuemax={100} style={{width: `${percentile}%`}}></div>
+const ColorResultComponent = ({ title, percentile }: SortResult) => (<>
+  <div className="progress mb-3 position-relative" style={{height: "50px"}}>
+    <div style={{height: "50px", color: "black", top: "50%", right: "50%"}}
+         className="fw-bold progress-text percentile-result">{title}: {percentile}%
     </div>
-  </>
-}
+    <div className="progress-bar bg-red" role="progressbar" aria-valuenow={percentile} aria-valuemin={0}
+         aria-valuemax={100} style={{width: `${percentile}%`}}></div>
+  </div>
+</>)
 
 type ColorDescriptionProps = { attribute: ColorAttributes }
-const ColorDescriptionComponent = ({ attribute }: ColorDescriptionProps) => {
-  const { description } = attribute;
-  const [isCollapsed, setCollapsed] = useState(true)
-
-  return isCollapsed
-    ? <button className="rounded w-100 mb-3" onClick={() => setCollapsed(!isCollapsed)}>Cliquez pour voir son descriptif</button>
-    : <>
+const ColorDescriptionComponent = ({ attribute }: ColorDescriptionProps) => (<>
+  <div className="mb-5">
+    <h2 className="text-center mb-5" style={{color: attribute.textColor}}>{attribute.title}</h2>
+    <div className="fw-bold">Fonctionnement en amour</div>
+    <p style={{textAlign: 'justify'}}>{attribute.description.behaviorInLove}</p>
+    <div className="fw-bold">Forces en relation amoureuse</div>
+    <p style={{textAlign: 'justify'}}>{attribute.description.relationshipStrength}</p>
+    <div className="fw-bold">Zones de difficult&eacute; et de challenge</div>
+    <p style={{textAlign: 'justify'}}>{attribute.description.relationshipWeakness}</p>
+  </div>
+  {
+    !attribute.description.advises.length ? <></> :
       <div className="mb-5">
-        <h2 className="text-center mb-5" style={{color: attribute.textColor}}>{attribute.title}</h2>
-        <div className="fw-bold">Fonctionnement en amour</div>
-        <p style={{textAlign: 'justify'}}>{description.behaviorInLove}</p>
-        <div className="fw-bold">Forces en relation amoureuse</div>
-        <p style={{textAlign: 'justify'}}>{description.relationshipStrength}</p>
-        <div className="fw-bold">Zones de difficult&eacute; et de challenge</div>
-        <p style={{textAlign: 'justify'}}>{description.relationshipWeakness}</p>
-      </div>
-      {
-        !description.advises.length ? <></> :
-          <div className="mb-5">
-            <div style={{color: attribute.textColor}} className="fs-3 mb-3">{description.advises.length} conseils pour am&eacute;liorer vos
-              relations amoureuses
+        <div style={{color: attribute.textColor}} className="fs-3 mb-3">{attribute.description.advises.length} conseils pour am&eacute;liorer vos
+          relations amoureuses
+        </div>
+        {
+          attribute.description.advises.map((advise, i) => (
+            <div key={`${attribute.textColor}-advice-${i}`}>
+              <div className="mb-3 fw-bold">{i + 1}. {advise.title}</div>
+              <div className="mb-3">{advise.content}</div>
             </div>
-            {
-              description.advises.map((advise, i) => (
-                <div key={`${attribute.textColor}-advice-${i}`}>
-                  <div className="mb-3 fw-bold">{i + 1}. {advise.title}</div>
-                  <div className="mb-3">{advise.content}</div>
-                </div>
-              ))
-            }
-            <button className="rounded w-100 mb-3" onClick={() => setCollapsed(!isCollapsed)}>Cliquez pour fermer le descriptif</button>
-          </div>
-      }
-    </>
+          ))
+        }
+      </div>
+  }
+</>)
+
+const HeaderComponent = () => (<>
+  <h3 className="mb-5 text-center">Bravo !</h3>
+  <h2 className="mb-5 text-center">Voici vos r&eacute;sultats</h2>
+  <img className="img-fluid w-100 my-5" alt="Resultats"
+       src="https://app.evalandgo.com/files/img_user/admin_550602/people-gf4565b6bd_1920(1).jpg" />
+</>)
+
+type FooterType = {
+  colorSpecificities: ColorSpecificities
 }
+const FooterComponent = ({ colorSpecificities }: FooterType) => (<>
+  <p className="fs-3 fw-bold mt-5">Prenez en compte vos 3 couleurs principales. Ce sont elles qui d&eacute;terminent
+    votre personnalit&eacute;.</p>
+  <p className="fs-3 text-center fw-bold">Qui sont les 6 profils amoureux ?</p>
+  {
+    COLORS.map((color) =>
+      (<ColorDescriptionComponent attribute={colorSpecificities[color]} key={color}/>))
+  }
+  <div className="fw-bold mb-3">F&eacute;licitations pour avoir d&eacute;couvert votre profil amoureux ! 🎉</div>
+  <div>
+    Vous avez maintenant une meilleure compr&eacute;hension de votre personnalit&eacute; amoureuse. Mais pourquoi
+    s'arr&ecirc;ter l&agrave; ?
+  </div>
+  <br/><br/>
+  <div>💡 Vous souhaitez aller plus loin ?</div>
+
+  <div className="row row-cols-1 row-cols-md-3 g-4">
+    <div className="col">
+      <div className="card h-100">
+        <img
+          src="https://app.evalandgo.com/files/img_user/admin_550602/Capture%20d%E2%80%99e%CC%81cran%202024-09-05%20a%CC%80%2011_26_45.png"
+          className="card-img-top buy-more" alt="..."/>
+        <div className="card-body">
+          <p className="card-text">
+            Explorez en profondeur votre profil pour mieux comprendre vos comportements et désirs amoureux.
+            Apprenez à vous aimer pour être mieux aimé(e).
+          </p>
+        </div>
+        <div className="card-footer">
+          <a href="https://objectif-love.com/connaissance-de-soi" target="_parent"
+             className="btn-link bg-red d-block text-decoration-none py-1 text-center">Acc&eacute;der</a>
+        </div>
+      </div>
+    </div>
+    <div className="col">
+      <div className="card h-100">
+        <img
+          src="https://app.evalandgo.com/files/img_user/admin_550602/Capture%20d%E2%80%99e%CC%81cran%202024-09-05%20a%CC%80%2011_26_55(1).png"
+          className="card-img-top buy-more" alt="..."/>
+        <div className="card-body">
+          <p className="card-text">
+            Découvrez avec qui vous êtes vraiment compatible.
+            Identifiez les forces et défis dans vos relations pour bâtir une histoire d'amour solide et épanouissante.
+          </p>
+        </div>
+        <div className="card-footer">
+          <a href="https://objectif-love.com/compatibilites-amoureuses" target="_parent"
+             className="btn-link bg-red d-block text-decoration-none py-1 text-center">Acc&eacute;der</a>
+        </div>
+      </div>
+    </div>
+    <div className="col">
+      <div className="card h-100">
+        <img
+          src="https://app.evalandgo.com/files/img_user/admin_550602/Capture%20d%E2%80%99e%CC%81cran%202024-09-05%20a%CC%80%2011_27_11.png"
+          className="card-img-top buy-more" alt="..."/>
+        <div className="card-body">
+          <p className="card-text">
+            Offrez-vous ou &agrave; votre partenaire un moment unique gr&acirc;ce &agrave; nos coffrets cadeaux remplis
+            de
+            surprises pour renforcer votre complicit&eacute; et pimenter votre relation.
+          </p>
+        </div>
+        <div className="card-footer">
+          <a href="https://objectif-love.com/coffrets-cadeaux" target="_parent"
+             className="btn-link bg-red d-block text-decoration-none py-1 text-center">Acc&eacute;der</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</>)
 
 type Props = {
   percentilePerColor: GroupByColor<number>
   colorSpecificities: ColorSpecificities
 }
 
-export default function ResultsComponent({ percentilePerColor, colorSpecificities }: Props) {
+export default function ResultsComponent({percentilePerColor, colorSpecificities}: Props) {
   return <>
-    <h3 className="mb-5 text-center">Bravo !</h3>
-    <h2 className="mb-5 text-center">Voici vos r&eacute;sultats</h2>
+    <HeaderComponent/>
     {
-      COLORS.map((color) => {
-        const percentile = percentilePerColor[color]
-
-        return (<div key={color}>
-          <ColorResultComponent title={colorSpecificities[color].title} percentile={percentile}/>
-          <ColorDescriptionComponent attribute={colorSpecificities[color]} />
-        </div>)
-      })
+      COLORS.map((color) =>
+        (<ColorResultComponent title={colorSpecificities[color].title} percentile={percentilePerColor[color]}
+                               key={color}/>))
     }
+    <FooterComponent colorSpecificities={colorSpecificities}/>
   </>
 }
